64875447952d872570b48971c34738e30a49b01c
GiulioDangelo <dangelogiulio48@gmail.com>
2023-07-10 17:14:05 +0200

    push

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // belongsTo si usa nel model della tabella che ha la chiave esterna, di conseguenza quella che sta dalla parte del molti
+    public function type(){
+        return $this->belongsTo(Type::class);
+    }
 }
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Type extends Model
 {
     use HasFactory;
+
+    // hasMany si usa sul model della tabella che NON ha la chiave esterna in una relazione uno a molti
+    // hasOone si usa sul model della tabella che NON ha la chiave esterna in una relazione uno a uno
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
 }
